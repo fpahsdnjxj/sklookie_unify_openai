ab27b470f5defdea37e0a21eb1863a6eea218a43
fpahsdnjxj <enjin0222@naver.com>
2025-01-12 01:36:08 +0900

    agent

--- chat_answer.py
+++ chat_answer.py
@@ -1,11 +1,11 @@
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
-from typing_extensions import List, TypedDict
-from langchain_core.documents import Document
-from langgraph.graph import START, StateGraph
 from langchain_openai import ChatOpenAI
-import os
+from langchain_core.tools import tool
+from langchain_core.messages import HumanMessage
+from langgraph.prebuilt import create_react_agent
 import streamlit as st
+import os
 
 from prompts import prompt
 
@@ -15,41 +15,35 @@ embeddings = OpenAIEmbeddings(model="text-embedding-3-large", openai_api_key=api
 db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "./2023_pdf"))
 db_2023 = FAISS.load_local(db_path, embeddings, allow_dangerous_deserialization=True)
 
-
 llm = ChatOpenAI(
-    model="gpt-4o",
-    temperature=0.0,
-    openai_api_key=api_key
+    model="gpt-4o-mini",
+    temperature=0.0
 )
 
-class State(TypedDict):
-    question: str
-    context: List[Document]
-    answer: str
-
-def retrieve(state: State):
-    if not state["question"] or not isinstance(state["question"], str):
-        raise ValueError("The question must be a non-empty string.")
-    retrieved_docs = db_2023.similarity_search(state["question"])
+@tool
+def search_yoram_2023(search_word):
+    """Retrieve information from 2023 sogang university Catalog return context from catalog"""
+    retrieved_docs = db_2023.similarity_search(search_word)
     return {"context": retrieved_docs}
 
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
 
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
-graph = graph_builder.compile()
+def get_answer_from_agent(question):
+    tools=[search_yoram_2023]
+    llm_with_tools=llm.bind_tools(tools)
+    agent_executor=create_react_agent(llm_with_tools, tools)
+    for chunk in agent_executor.stream(
+        {"messages": [HumanMessage(content=question)]}
+    ):
+        final_message=chunk
+    return final_message["agent"]["messages"][0].content
 
 def main():
     st.title("유니피")
     st.write("안녕하세요 저는 유니피에요. 원하는 질문을 입력해주세요.")
     user_input = st.text_input("질문을 입력하세요")
     if st.button("Send"):
-        response = graph.invoke({"question": user_input})
-        st.write(response["answer"])
+        response = get_answer_from_agent(user_input)
+        st.write(response)
         
 
 if __name__ == "__main__":
